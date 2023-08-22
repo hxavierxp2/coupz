0bf8f91bf1fc423db9c170f8273d07c8a47de400
LeHenriqueAlmeida <henriquexavier@lelabs.com.br>
2023-08-21 22:16:53 -0300

    Build

---
+++ build/precache-manifest.00aa28b1b1c02d04acbf073e814de40c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ece50a99da284b4604ee0bcab61ddb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a50177b2dcef46f2dde2",
+    "url": "./static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "619783da967e693243a9",
+    "url": "./static/css/main.b0043f96.chunk.css"
+  },
+  {
+    "revision": "a50177b2dcef46f2dde2",
+    "url": "./static/js/2.5476d5e1.chunk.js"
+  },
+  {
+    "revision": "5a8f13fa0c912d44e4f1b61e292004fc",
+    "url": "./static/js/2.5476d5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619783da967e693243a9",
+    "url": "./static/js/main.caf5d52b.chunk.js"
+  },
+  {
+    "revision": "0785584a061b25d6e41d",
+    "url": "./static/js/runtime-main.0ee2d0c3.js"
+  }
+]);
